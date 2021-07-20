3848b0080bd1c0a86cc41cadf9b156f6219fdb08
tYohannes <teklegy@gmail.com>
2021-07-20 16:19:39 +0300

    Code refactored to implement EJS templating engine.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# MyNodeBasedBlogPost
+# My NodeJS Based Blog Post
+
+# Install NodeJS
+
+https://nodejs.org/en/
+$ sudo apt install nodejs
+
+# Install Express
+
+https://expressjs.com/
+$ npm install express --sav
+
+# Install EJS Templating Engine
+
+https://ejs.co/
+$ npm install ejs
--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
 
-const app = express()
-app.use(express.static('public'))
+const app = express();
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
-})
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/about', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
-})
+app.get("/", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
+});
 
-app.get('/contact', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
-})
+app.get("/about", (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+  res.render("about");
+});
 
-app.get('/post', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
-})
-app.listen(4000, ()=> {
-    console.log('App Server started on port 4000...')
-})
+app.get("/contact", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "pages/contact.html"));
+  res.render("contact");
+});
+
+app.get("/post", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "pages/post.html"));
+  res.render("post");
+});
+app.listen(4000, () => {
+  console.log("App Server started on port 4000...");
+});
